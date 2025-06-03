ff67fd6a727022cf18f75b5270da9f9af65d2412
Mohamed Elkmeshi <95718825+MElkmeshi@users.noreply.github.com>
2025-06-03 18:54:42 +0300

    chore: initialize package.json with dependencies and scripts for proxy server

---
+++ app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+const app = express();
+const PORT = 8888;
+const API_SERVICE_URL = "https://jsonplaceholder.typicode.com";
+
+app.use(
+  createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+  })
+);
+app.listen(PORT, () => {
+  console.log(`Starting Proxy at http://localhost:${PORT}`);
+});
